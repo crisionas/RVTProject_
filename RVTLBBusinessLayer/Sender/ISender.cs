df10ee6e1e73a7e202d5d90d3acc15ff8ed8bb89
mrelectrq <tiora100bass@gmail.com>
2020-07-13 16:56:52 +0300

    Implemented VoteSender class

---
+++ RVTLBBusinessLayer/Sender/ISender.cs
@@ -0,0 +1,9 @@
+﻿using RVTLibrary.Objects;
+
+namespace RVTLBBusinessLayer.Sender
+{
+    public interface ISender
+    {
+        string Send(Node node, object package);
+    }
+}
---
+++ RVTLBBusinessLayer/Sender/VoteSender.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RVT_Block_lib.Models;
+using RVTLibrary.Objects;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RVTLBBusinessLayer.Sender
+{
+    public class VoteSender : ISender
+    {
+        public readonly HttpClient client;
+        public string Send(Node node, object package)
+        {
+            var data_req = JsonConvert.SerializeObject((NodeVoteMessage)package);
+            var content = new StringContent(data_req, Encoding.UTF8, "application/json");
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+                AllowAutoRedirect = true
+            };
+
+            var client = new HttpClient(handler);
+            client.BaseAddress = new Uri(node.Url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = client.PostAsync("api/Vote", content);
+
+            return response.Result.Content.ReadAsStringAsync().Result;
+
+        }
+    }
+}
